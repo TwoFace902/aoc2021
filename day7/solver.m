cb0e429c1dd91e77d0adcb0539c5dc3be1fb3ab4
TwoFace902 <brandon.g.hong@vanderbilt.edu>
2021-12-06 23:14:24 -0600

    day 7 pog

---
+++ day7/solver.m
@@ -0,0 +1,22 @@
+chuck = readmatrix('input.txt');
+temp = chuck;
+sum = 0;
+buck = zeros(size(chuck,2));
+for i = 1:size(chuck,2)
+    sum = 0;
+    for j = 1:size(chuck,2)
+        sum = sum + (abs(chuck(j) - chuck(i)));
+    end
+    temp(i) = sum;
+end
+min(temp)
+sneed = zeros(size(chuck,2));
+for i = 1:size(chuck,2)
+    sum = 0;
+    for j = 1:size(chuck,2)
+        sum = sum + ((abs(chuck(j) - chuck(i))) * (abs(chuck(j) - chuck(i)) + 1) / 2);
+    end
+    sneed(i) = sum;
+end
+sneed = min(sneed);
+sneed(1)
