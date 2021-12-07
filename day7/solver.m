fd67f16e365dc415e5068bcbd0841550dc4a288f
TwoFace902 <brandon.g.hong@vanderbilt.edu>
2021-12-07 13:10:58 -0600

    Added non-bruteforce solver.

--- day7/solver.m
+++ day7/solver.m
@@ -1,24 +1,26 @@
 chuck = readmatrix('input.txt');
-temp = chuck;
-sum = 0;
-%Calc part 1 sum brute-force.
-for i = 1:size(chuck,2)
-    sum = 0;
-    for j = 1:size(chuck,2)
-        sum = sum + (abs(chuck(j) - chuck(i)));
-    end
-    temp(i) = sum;
+sum = [0;0];
+%Calc part 1
+middle = median(chuck,'all');
+floorer = abs(chuck - floor(middle));
+for i = 1:size(floorer,2)
+    sum(1) = sum(1) + floorer(i);
 end
-min(temp)
+ceiler = abs(chuck - ceil(middle));
+for i = 1:size(ceiler,2)
+    sum(2) = sum(2) + ceiler(i);
+end
+fprintf("Part 1 soln: %d\n",min(sum));
 
-%Calc part 2 sum brute force.
-sneed = zeros(size(chuck,2));
-for i = 1:size(chuck,2)
-    sum = 0;
-    for j = 1:size(chuck,2)
-        sum = sum + ((abs(chuck(j) - chuck(i))) * (abs(chuck(j) - chuck(i)) + 1) / 2);
-    end
-    sneed(i) = sum;
+%Calc part 2
+sum = [0;0];
+meany = mean(chuck,'all');
+floorer = abs(chuck - floor(meany));
+for i = 1:size(floorer,2)
+    sum(1) = sum(1) + ((floorer(i) * (floorer(i) + 1)) / 2);
 end
-sneed = min(sneed);
-sneed(1)
+ceiler = abs(chuck - ceil(meany));
+for i = 1:size(ceiler,2)
+    sum(2) = sum(2) + ((ceiler(i) * (ceiler(i) + 1)) / 2);
+end
+fprintf("Part 2 soln: %d\n",min(sum));
